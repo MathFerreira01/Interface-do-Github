a323966400dbf661765e793391675b835b43c4cb
MathFerreira01 <ferreiramatheus124@gmail.com>
2022-03-04 16:20:49 -0300

    autenticação api github

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,13 +1,22 @@
-import React, {useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
-import { Container, Main, LeftSide, RightSide, Repos, CalendarHeading, RepoIcon, Tab } from './styles';
+import {
+  Container,
+  Main,
+  LeftSide,
+  RightSide,
+  Repos,
+  CalendarHeading,
+  RepoIcon,
+  Tab,
+} from "./styles";
 
-import ProfileDate from '../../components/ProfileData';
-import RepoCard from '../../components/RepoCard';
-import RandomCalendar from '../../components/RandomCalendar';
+import ProfileDate from "../../components/ProfileData";
+import RepoCard from "../../components/RepoCard";
+import RandomCalendar from "../../components/RandomCalendar";
 
-import { APIRepo, APIUser } from '../../@types';
+import { APIRepo, APIUser } from "../../@types";
 
 interface Data {
   user?: APIUser;
@@ -16,64 +25,63 @@ interface Data {
 }
 
 const Profile: React.FC = () => {
-
-  const { username = 'MathFerreira01'} = useParams();
+  const { username = "MathFerreira01" } = useParams();
   const [data, setData] = useState<Data>();
 
-  useEffect (() => {
-    Promise.all ([
+  useEffect(() => {
+    Promise.all([
       fetch(`https://api.github.com/users/${username}`),
-      fetch(`https://api.github.com/users/${username}/repos`)
+      fetch(`https://api.github.com/users/${username}/repos`),
     ]).then(async (responses) => {
       const [userResponse, reposResponse] = responses;
 
-      if(userResponse.status === 404) {
-        setData({error: 'User not found!'})
+      if (userResponse.status === 404) {
+        setData({ error: "User not found!" });
         return;
       }
 
-      const user = await userResponse.json()
-      const repos = await reposResponse.json()
+      const user = await userResponse.json();
+      const repos = await reposResponse.json();
 
-      const shuffledRepos = repos.sort(() => 0.50 - Math.random())
+      const shuffledRepos = repos.sort(() => 0.5 - Math.random());
       const slicedRepos = shuffledRepos.slice(0, 6);
 
       setData({
-        user:user,
+        user,
         repos: slicedRepos,
-      })
-    })
+      });
+    });
   }, [username]);
 
-  if(data?.error) {
-    return<h1>{data.error}</h1>
+  if (data?.error) {
+    return <h1>{data.error}</h1>;
   }
 
   if (!data?.user || !data?.repos) {
-    return <h1>Loading...</h1>
+    return <h1>Loading...</h1>;
   }
 
   const TabContent = () => (
-    <div className='content'>
-      <RepoIcon/>
-      <span className='label'>Repositories</span>
-      <span className='number'>{data.user?.public_repos}</span>
+    <div className="content">
+      <RepoIcon />
+      <span className="label">Repositories</span>
+      <span className="number">{data.user?.public_repos}</span>
     </div>
-  )
+  );
 
   return (
     <Container>
-      <Tab className='descktop'>
-        <div className='wrapper'>
-          <span className='offset'/>
-          <TabContent/>
+      <Tab className="descktop">
+        <div className="wrapper">
+          <span className="offset" />
+          <TabContent />
         </div>
-        <span className='line'/>
+        <span className="line" />
       </Tab>
 
       <Main>
         <LeftSide>
-          <ProfileDate 
+          <ProfileDate
             username={data.user.login}
             name={data.user.name}
             avatarUrl={data.user.avatar_url}
@@ -83,43 +91,42 @@ const Profile: React.FC = () => {
             location={data.user.location}
             email={data.user.email}
             blog={data.user.blog}
-
           />
         </LeftSide>
 
         <RightSide>
-        <Tab className='mobile'>
-          <TabContent/>
-          <span className='line'/>
-        </Tab>
+          <Tab className="mobile">
+            <TabContent />
+            <span className="line" />
+          </Tab>
 
           <Repos>
             <h2>Random repos</h2>
 
             <div>
-                {data.repos.map ((item) => (
-                  <RepoCard
-                    key={item.name}
-                    username={item.owner.login}
-                    reponame={item.name}
-                    description={item.description}
-                    language={item.language}
-                    stars={item.stargazers_count}
-                    forks={item.forks}
-                  />
-                ))}
+              {data.repos.map((item) => (
+                <RepoCard
+                  key={item.name}
+                  username={item.owner.login}
+                  reponame={item.name}
+                  description={item.description}
+                  language={item.language}
+                  stars={item.stargazers_count}
+                  forks={item.forks}
+                />
+              ))}
             </div>
           </Repos>
-          
+
           <CalendarHeading>
             Random calendar (do not represent actual data)
           </CalendarHeading>
 
-          <RandomCalendar/>
+          <RandomCalendar />
         </RightSide>
       </Main>
     </Container>
-  )
-}
+  );
+};
 
 export default Profile;
